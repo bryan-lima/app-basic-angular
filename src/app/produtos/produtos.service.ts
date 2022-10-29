bdf315a0f2a8809e680706cf8323efbe0a3fcfa3
Bryan <bryanlima200@outlook.com>
2022-10-28 23:57:15 -0300

    Implementado consumo de service através do componente e adicionado imagens

--- src/app/produtos/produtos.service.ts
+++ src/app/produtos/produtos.service.ts
@@ -12,6 +12,6 @@ constructor(private http: HttpClient) {}
     protected UrlServiceV1: string = "http://localhost:3000";
 
     obterProdutos() : Observable<Produto[]> {
-        return this.http.get<Produto[]>(this.UrlServiceV1 + "produtos");
+        return this.http.get<Produto[]>(this.UrlServiceV1 + "/produtos");
     }
 }
